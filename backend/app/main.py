7587bd6bf02c3b98bbd7ed5460fee695c4a2c931
Skyler <mclainskyler@gmail.com>
2025-05-20 10:22:12 -0400

    getting backend prepped

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from .db import SessionLocal
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
