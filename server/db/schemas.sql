076235a128825c88a10d8737000cbbb4b60528ba
Skyler <mclainskyler@gmail.com>
2025-05-21 12:38:48 -0400

    i hate git

--- server/db/schemas.sql
+++ server/db/schemas.sql
@@ -13,9 +13,9 @@ CREATE TABLE users (
     last_name VARCHAR(32) NOT NULL,
     username VARCHAR(16) NOT NULL,
     email VARCHAR(64) NOT NULL,
-    password_hash TEXT NOT NULL,
+    password_hash TEXT NOT NULL, --???
     bio VARCHAR(255),
-    rating int,
+    rating int DEFAULT NULL,
     profile_pic TEXT,
     create_date DATE NOT NULL DEFAULT NOW()
 );
@@ -63,29 +63,29 @@ CREATE TABLE messages (
 
 INSERT INTO users (first_name, last_name, username, email, password_hash, bio, rating, profile_pic, create_date)
 VALUES
-    ('Cayla', 'Thompson', 'blueeyed95', 'caylaleann18@gmail.com', '12345678', 'Looking for piano lessons!', '', '', '2025-05-20'),
-    ('Cayla', 'Johnson', 'greeneyed95', 'caylaleann19@gmail.com', '12345678', 'Looking for rollerblading lessons, please', '', '', '2025-05-20'),
-    ('Cayla', 'Wilson', 'greyeyed95', 'caylaleann20@gmail.com', '12345678', 'I like turtles.', '', '', '2025-05-20'),
-    ('Cayla', 'Davidson', 'yelloweyed95', 'caylaleann21@gmail.com', '12345678', 'What?', '', '', '2025-05-20'),
-    ('Cayla', 'Simpson', 'redeyed95', 'caylaleann22@gmail.com', '12345678', '', '', '', '2025-05-20'),
-    ('Cayla', 'Dawson', 'pinkeyed95', 'caylaleann23@gmail.com', '12345678', '', '', '', '2025-05-20'),
-    ('Cayla', 'Bobbyson', 'beigeeyed95', 'caylaleann24@gmail.com', '12345678', '', '', '', '2025-05-20'),
-    ('Cayla', 'Billyson', 'wideeyed95', 'caylaleann25@gmail.com', '12345678', '', '', '', '2025-05-20'),
-    ('Cayla', 'Jayson', 'buggyeyed95', 'caylaleann26@gmail.com', '12345678', '', '', '', '2025-05-20');
+    ('Cayla', 'Thompson', 'blueeyed95', 'caylaleann18@gmail.com', '12345678', 'Looking for piano lessons!', NULL, '', '2025-05-20'),
+    ('Cayla', 'Johnson', 'greeneyed95', 'caylaleann19@gmail.com', '12345678', 'Looking for rollerblading lessons, please', NULL, '', '2025-05-20'),
+    ('Cayla', 'Wilson', 'greyeyed95', 'caylaleann20@gmail.com', '12345678', 'I like turtles.', NULL, '', '2025-05-20'),
+    ('Cayla', 'Davidson', 'yelloweyed95', 'caylaleann21@gmail.com', '12345678', 'What?', NULL, '', '2025-05-20'),
+    ('Cayla', 'Simpson', 'redeyed95', 'caylaleann22@gmail.com', '12345678', '', NULL, '', '2025-05-20'),
+    ('Cayla', 'Dawson', 'pinkeyed95', 'caylaleann23@gmail.com', '12345678', '', NULL, '', '2025-05-20'),
+    ('Cayla', 'Bobbyson', 'beigeeyed95', 'caylaleann24@gmail.com', '12345678', '', NULL, '', '2025-05-20'),
+    ('Cayla', 'Billyson', 'wideeyed95', 'caylaleann25@gmail.com', '12345678', '', NULL, '', '2025-05-20'),
+    ('Cayla', 'Jayson', 'buggyeyed95', 'caylaleann26@gmail.com', '12345678', '', NULL, '', '2025-05-20');
 
 INSERT INTO categories (name) VALUES
-    ('Music & Performing Arts'),
-    ('Creative Crafts'),
-    ('Trades & Technical'),
-    ('Culinary & Baking'),
-    ('Fitness & Sports'),
-    ('Language & Communication'),
-    ('Academic & Tutoring'),
-    ('Gaming & Digital'),
-    ('Wellness & Lifestyle'),
-    ('Automotive & Mechanics'),
-    ('Outdoor & Survival'),
-    ('Other');
+    ('Music & Performing Arts'), --1
+    ('Creative Crafts'), --2
+    ('Trades & Technical'), --3
+    ('Culinary & Baking'), --4
+    ('Fitness & Sports'), --5
+    ('Language & Communication'), --6
+    ('Academic & Tutoring'), --7
+    ('Gaming & Digital'), --8
+    ('Wellness & Lifestyle'), --9
+    ('Automotive & Mechanics'), --10
+    ('Outdoor & Survival'), --11
+    ('Other'); --12
 
 INSERT INTO skills (name, category_id) VALUES
     ('Guitar', 1),
@@ -120,7 +120,7 @@ VALUES
 
 INSERT INTO messages (sender_id, receiver_id, content, timestamp, is_read, connection_id)
 VALUES
-    -- log examples for users 1 and 3 also set latest msg to unread -- also ensured that the connection matches with existing actual connections
+    -- log examples for users 1 and 3 also set latest msg to unread
     (1, 3, 'Hey! How’s it going?', NOW(), TRUE, 1),
     (3, 1, 'Good! fiddlin with my sick af guitar.', NOW(), TRUE, 1),
     (1, 3, 'Awesome, lets boogie bro - mind teaching me?', NOW(), FALSE, 1),
