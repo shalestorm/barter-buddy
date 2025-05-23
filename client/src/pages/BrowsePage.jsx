340ade73748455ed088b15f9ef38cc607bfed8cc
Cayla <caylaleann18@gmail.com>
2025-05-23 12:51:51 -0400

    starter

---
+++ client/src/pages/BrowsePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BrowsePage = () => {
+    return (
+        <div className="browse-page">
+            <h2>Browse Knowledge Sharers</h2>
+            <p>Explore users who are offering and looking to learn new skills.</p>
+        </div>
+    );
+};
+
+export default BrowsePage;
---
+++ client/src/pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatPage = () => {
+    return (
+        <div className="chat-page">
+            <h2>Chat</h2>
+            <p>This is where you can chat with other users.</p>
+        </div>
+    );
+};
+
+export default ChatPage;
