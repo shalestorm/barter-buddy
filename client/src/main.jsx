2184cbe19cecbd690aabbfe9ca298ba2792ad94e
Ricardo <rtizonc@gmail.com>
2025-05-23 08:36:43 -0400

    Initial React routes setup (discuss)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client'  // for react router
+import { BrowserRouter } from 'react-router' // for react router
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
