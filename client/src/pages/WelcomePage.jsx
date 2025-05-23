fe0b08c81e7e74f932a573afd346e6897625751a
Cayla <caylaleann18@gmail.com>
2025-05-23 12:41:17 -0400

    new pages

---
+++ client/src/pages/WelcomePage.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import logo from '../assets/bb_new.png';
+
+const WelcomePage = () => {
+    useEffect(() => {
+        const colors = [
+            [25, 65, 130],
+            [255, 204, 0],
+            [25, 65, 130],
+            [255, 204, 0]
+        ];
+
+        let step = 0;
+        const colorIndices = [0, 1, 2, 3];
+        const gradientSpeed = 0.002;
+
+        const updateGradient = () => {
+            const c0_0 = colors[colorIndices[0]];
+            const c0_1 = colors[colorIndices[1]];
+            const c1_0 = colors[colorIndices[2]];
+            const c1_1 = colors[colorIndices[3]];
+
+            const istep = 1 - step;
+            const r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+            const g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+            const b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+            const color1 = `rgb(${r1},${g1},${b1})`;
+
+            const r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+            const g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+            const b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+            const color2 = `rgb(${r2},${g2},${b2})`;
+
+            const gradientEl = document.getElementById("gradient");
+            if (gradientEl) {
+                gradientEl.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+            }
+
+            step += gradientSpeed;
+            if (step >= 1) {
+                step %= 1;
+                colorIndices[0] = colorIndices[1];
+                colorIndices[2] = colorIndices[3];
+
+                colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+                colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+            }
+
+            requestAnimationFrame(updateGradient);
+        };
+
+        updateGradient();
+    }, []);
+
+    return (
+        <div id="gradient" className="fade-in">
+            <img src={logo} alt="Barter Buddy Logo" className="header-logo" />
+            <h1 className="magic-title">Welcome to Barter Buddy!</h1>
+            <p className="magic-subtitle">The Free Exchange of Knowledge</p>
+
+
+            <div className="button-container">
+                <button className="magic-button">New to Barter Buddy?</button>
+                <button className="magic-button">Log In</button>
+                <button className="magic-button">Forgot Password?</button>
+            </div>
+        </div>
+    );
+
+};
+
+export default WelcomePage;
