2ecf4228b4ebb1440ff4143dabdcad26f59e377f
Skyler <mclainskyler@gmail.com>
2025-05-30 12:33:13 -0400

    echoing previous

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router'
 import DashboardPage from './pages/DashboardPage'
-import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import MessagesPage from './pages/MessagesPage'
@@ -22,7 +21,6 @@ function App() {
           <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/dashboard' element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-          <Route path='/chat/:userId' element={<ProtectedRoute><ChatPage /></ProtectedRoute>} />
           <Route path='/founders' element={<FoundersLogPage />} />
           <Route path='/messages/:userId' element={<ProtectedRoute><MessagesPage /></ProtectedRoute>} />
           <Route path='/profile/:userId' element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
