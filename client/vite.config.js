ebfc600de31b471fa8704ccc0274fdaf950cc963
Skyler <mclainskyler@gmail.com>
2025-06-11 21:25:49 -0400

    ya

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/users': 'http://localhost:8000',
-      '/connections': 'http://localhost:8000',
-      '/connection_requests': 'http://localhost:8000',
-      '/user-skills': 'http://localhost:8000',
-    }
-  }
 })
